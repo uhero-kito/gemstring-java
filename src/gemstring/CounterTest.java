b87fcb8b0b66e493a82a6631699db23aa67e8ab2
uhero_kito <takayuki.kito@uhero.co.jp>
2014-02-13 19:55:36 +0900

    Added final keyword

--- src/gemstring/CounterTest.java
+++ src/gemstring/CounterTest.java
@@ -23,11 +23,11 @@ public class CounterTest {
         System.out.println("gems   : " + args[0]);
         System.out.println("target : " + args[1]);
         final String target = args[1];
-        Counter c = new Counter(new PatternGenerator(args[0]), target);
-        ForkJoinPool pool = new ForkJoinPool(32);
-        long t1 = System.nanoTime();
+        final Counter c = new Counter(new PatternGenerator(args[0]), target);
+        final ForkJoinPool pool = new ForkJoinPool(32);
+        final long t1 = System.nanoTime();
         System.out.println(pool.invoke(c) + ":" + target);
-        long t2 = System.nanoTime();
+        final long t2 = System.nanoTime();
         System.out.println("Time: " + (t2 - t1));
     }
 }
